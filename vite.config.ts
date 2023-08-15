2e4ace725433f47e813eebacca1c288296ae1fc5
keneanung <keneanung@gmail.com>
2023-08-15 23:06:11 +0200

    use manual chunks for rollup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nssc/'
+  base: '/nssc/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.indexOf("node_modules") > -1) {
+            if (id.indexOf("@mui") > -1) {
+              return "vendor_mui";
+            }
+
+            return "vendor"; // all other package goes here
+          }
+        },
+      }
+    }
+  }
 })
